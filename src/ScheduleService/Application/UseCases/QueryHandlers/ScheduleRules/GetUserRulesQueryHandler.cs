d98e0b972650a3aa459b608b10d46a39a013ddc4
antko35 <kovsheranton@gmail.com>
2025-02-06 18:20:59 +0300

    in pocess

---
+++ src/ScheduleService/Application/UseCases/QueryHandlers/ScheduleRules/GetUserRulesQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using ScheduleService.Application.Extensions;
+using ScheduleService.Application.UseCases.Queries.ScheduleRules;
+using ScheduleService.DataAccess.Repository;
+using ScheduleService.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleService.Application.UseCases.QueryHandlers.ScheduleRules
+{
+    public class GetUserRulesQueryHandler
+        : IRequestHandler<GetUserRulesQuery, UserScheduleRules>
+    {
+        private readonly IUserRuleRepository userRuleRepository;
+
+        public GetUserRulesQueryHandler(IUserRuleRepository userRuleRepository)
+        {
+            this.userRuleRepository = userRuleRepository;
+        }
+
+        public async Task<UserScheduleRules> Handle(GetUserRulesQuery request, CancellationToken cancellationToken)
+        {
+            var monthName = new DateTime(request.Year, request.Month, 1)
+                .ToString("MMMM")
+                .ToLower();
+
+            var response = await userRuleRepository
+                .GetMonthScheduleRules(
+                request.UserId,
+                request.DepartmentId,
+                monthName,
+                request.Year);
+
+            response.EnsureExists("Rules not found");
+
+            return response;
+        }
+    }
+}
--- src/ScheduleService/ScheduleService.API/Controllers/ScheduleRulesController.cs
+++ src/ScheduleService/ScheduleService.API/Controllers/ScheduleRulesController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using ScheduleService.Application.UseCases.Commands.ScheduleRules;
+using ScheduleService.Application.UseCases.Queries.ScheduleRules;
 
 namespace ScheduleService.API.Controllers
 {
@@ -16,7 +17,25 @@ namespace ScheduleService.API.Controllers
         }
 
         /// <summary>
-        ///
+        /// Get ruler for schedule generation.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="departmentId"></param>
+        /// <param name="year"></param>
+        /// <param name="month"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("rules/{userId}/{departmentId}/{year:int}/{month:int}")]
+        public async Task<IActionResult> GetUserRules(string userId, string departmentId, int year, int month)
+        {
+            var result = await mediator.Send(new GetUserRulesQuery(userId, departmentId, month, year));
+
+            return Ok(result);
+        }
+
+        /// <summary>
+        /// Auto creating userScheduleRules for schedule generation.
+        /// Creating after adding user to department(in user management service).
         /// </summary>
         /// <param name="command"></param>
         /// <returns></returns>
@@ -25,6 +44,16 @@ namespace ScheduleService.API.Controllers
         {
             await mediator.Send(command);
 
+            return Ok();
+        }
+
+        /// <summary>
+        /// Set default rules for schedule rules.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> ClearRules()
+        {
             return Ok();
         }
     }
