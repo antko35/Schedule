385e67168880fb382082553fab1aa50608a0243a
antko35 <kovsheranton@gmail.com>
2025-02-26 11:01:03 +0300

    fix Commamd

--- src/ScheduleService/Application/UseCases/Commands/Schedule/AddWorkingSaturdayCommand.cs
+++ src/ScheduleService/Application/UseCases/Commands/Schedule/AddWorkingSaturdayCommand.cs
@@ -16,15 +16,5 @@ namespace ScheduleService.Application.UseCases.Commands.Schedule
         public WorkDay Saturday { get; set; }
 
         public List<WorkDay> WorkingDays { get; set; } = new List<WorkDay>();
-
-        public WorkDay DayOne { get; set; }
-
-        public WorkDay DayTwo { get; set; }
-
-        public WorkDay DayThree { get; set; }
-
-        public WorkDay DayFour { get; set; }
-
-        public WorkDay DayFive { get; set; }
     }
 }
